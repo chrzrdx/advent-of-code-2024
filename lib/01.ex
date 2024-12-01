7d0d5108d5d61b7128e8044d1f515e910893a2fe
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-01 11:35:14 -0700

    Minor improvements

--- lib/01.ex
+++ lib/01.ex
@@ -1,35 +1,32 @@
 defmodule AdventOfCode.Day1 do
   def solve_p1(filename) do
-    input = read_input(filename)
-
-    left = input |> Enum.map(fn [a, _] -> a end) |> Enum.sort()
-    right = input |> Enum.map(fn [_, b] -> b end) |> Enum.sort()
+    {left, right} = read_input(filename)
 
     # distance =
-    Enum.zip(left, right)
+    Enum.zip(Enum.sort(left), Enum.sort(right))
     |> Enum.map(fn {a, b} -> abs(a - b) end)
     |> Enum.sum()
   end
 
   def solve_p2(filename) do
-    input = read_input(filename)
+    {left, right} = read_input(filename)
+    freq = Enum.frequencies(right)
 
-    freq = Enum.frequencies_by(input, fn [_, b] -> b end)
-
-    input
-    |> Enum.map(fn [a, _] -> a * Map.get(freq, a, 0) end)
+    # similarity =
+    left
+    |> Enum.map(fn a -> a * Map.get(freq, a, 0) end)
     |> Enum.sum()
   end
 
-  defp read_input(filename) do
+  def read_input(filename) do
     filename
     |> File.read!()
-    |> String.split("\n")
+    |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
-      String.split(line, " ")
-      |> Enum.filter(&(String.length(&1) > 0))
-      |> Enum.map(&String.to_integer(&1))
+      line
+      |> String.split(" ", trim: true)
+      |> (fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end).()
     end)
-    |> Enum.filter(&(length(&1) == 2))
+    |> Enum.unzip()
   end
 end
