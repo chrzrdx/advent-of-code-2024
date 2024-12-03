a0c80c71092de08adad1d7bd3583082f8289e7ed
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-02 22:41:52 -0700

    Day 3: Parts 1 & 2

--- lib/03.ex
+++ lib/03.ex
@@ -1,22 +1,30 @@
 defmodule AdventOfCode.Day3 do
   def solve_p1(filename) do
-    read_input(filename) |> Enum.count()
-    1
+    read_input(filename)
+    |> parse()
   end
 
   def solve_p2(filename) do
-    read_input(filename) |> Enum.count()
-    1
+    read_input(filename)
+    |> remove_disabled_instructions()
+    |> parse()
+  end
+
+  defp parse(instructions) do
+    ~r/mul\(([0-9]{1,3}),([0-9]{1,3})\)/
+    |> Regex.scan(instructions)
+    |> Enum.map(fn [_, a, b] -> String.to_integer(a) * String.to_integer(b) end)
+    |> Enum.sum()
+  end
+
+  defp remove_disabled_instructions(instructions) do
+    ~r/don't\(\).*do\(\)/U
+    |> Regex.replace(instructions, "-")
   end
 
   defp read_input(filename) do
     filename
     |> File.read!()
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn line ->
-      line
-      |> String.split(" ", trim: true)
-      |> Enum.map(&String.to_integer(&1))
-    end)
+    |> String.replace("\n", "-")
   end
 end
--- test/03_test.exs
+++ test/03_test.exs
@@ -3,18 +3,18 @@ defmodule AdventOfCode.Day3Test do
   alias AdventOfCode.Day3
 
   test "p1: basic" do
-    assert Day3.solve_p1("test/02_tc_01.input") == 1
+    assert Day3.solve_p1("test/03_tc_01.input") == 161
   end
 
   test "p1: puzzle" do
-    assert Day3.solve_p1("test/02_tc_puzzle.input") == 1
+    assert Day3.solve_p1("test/03_tc_puzzle.input") == 178_794_710
   end
 
   test "p2: basic" do
-    assert Day3.solve_p2("test/02_tc_01.input") == 1
+    assert Day3.solve_p2("test/03_tc_02.input") == 48
   end
 
   test "p2: puzzle" do
-    assert Day3.solve_p2("test/02_tc_puzzle.input") == 1
+    assert Day3.solve_p2("test/03_tc_puzzle.input") == 76_729_637
   end
 end
