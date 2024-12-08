87f5c8766f410178a4c279d5ebc9e62d6639ca50
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-08 04:18:28 -0500

    Day 7: Part 2 (wip)

--- lib/07.ex
+++ lib/07.ex
@@ -1,26 +1,68 @@
 defmodule AdventOfCode.Day07 do
   def solve_p1(filename) do
     read_input(filename)
-    |> Enum.filter(&solvable?/1)
+    |> Enum.filter(&solvable_p1?/1)
     |> Enum.map(fn {target, _} -> target end)
     |> Enum.sum()
   end
 
   def solve_p2(filename) do
     read_input(filename)
-    1
+    |> Enum.filter(&solvable_p2?/1)
+    |> IO.inspect()
+    |> Enum.map(fn {target, _} -> target end)
+    |> Enum.sum()
   end
 
-  def solvable?({target, nums}) do
-    solvable?(target, 0, nums)
+  def solvable_p1?({target, [first | rest]}) do
+    solvable_p1?(target, first, rest)
   end
 
-  def solvable?(target, current, []), do: target == current
-  def solvable?(target, current, _nums) when current > target, do: false
+  def solvable_p1?(target, current, []), do: target == current
+  def solvable_p1?(target, current, _nums) when current > target, do: false
 
-  def solvable?(target, current, [top | rest]) do
-    solvable?(target, current * top, rest) or
-      solvable?(target, current + top, rest)
+  def solvable_p1?(target, current, [top | rest]) do
+    solvable_p1?(target, current * top, rest) or
+      solvable_p1?(target, current + top, rest)
+  end
+
+  def solvable_p2?({target, [first | rest]}) do
+    solvable_p2?(target, first, rest, [{:+, first}])
+  end
+
+  def solvable_p2?(target, current, [], _ops), do: target == current
+
+  def solvable_p2?(target, current, [top | rest] = nums, ops) do
+    IO.inspect({target, current, nums, ops, nil})
+
+    solvable_p2?(target, current * top, rest, [{:*, top} | ops]) or
+      solvable_p2?(target, current + top, rest, [{:+, top} | ops]) or
+      concat_and_solvable_p2?(target, current, nums, ops)
+  end
+
+  def concat_and_solvable_p2?(
+        target,
+        current,
+        [top | rest] = nums,
+        [{last_op, last_num} | rest_ops] = ops
+      ) do
+    joined = String.to_integer("#{last_num}#{top}")
+
+    IO.inspect({target, current, nums, ops, joined})
+
+    previous =
+      case last_op do
+        :* -> Kernel.div(current, last_num)
+        :+ -> current - last_num
+      end
+
+    case last_op do
+      :* ->
+        solvable_p2?(target, previous * joined, rest, [{:*, joined} | rest_ops])
+
+      :+ ->
+        solvable_p2?(target, previous + joined, rest, [{:+, joined} | rest_ops])
+    end
   end
 
   defp read_input(filename) do
--- test/07_test.exs
+++ test/07_test.exs
@@ -2,17 +2,18 @@ defmodule AdventOfCode.Day07Test do
   use ExUnit.Case
   alias AdventOfCode.Day07
 
+  @tag :skip
   test "p1: basic" do
     assert Day07.solve_p1("test/07_tc_01.input") == 3749
   end
 
+  @tag :skip
   test "p1: puzzle" do
     assert Day07.solve_p1("test/07_tc_puzzle.input") == 7_710_205_485_870
   end
 
-  @tag :skip
   test "p2: basic" do
-    assert Day07.solve_p2("test/07_tc_02.input") == 1
+    assert Day07.solve_p2("test/07_tc_01.input") == 11387
   end
 
   @tag :skip
