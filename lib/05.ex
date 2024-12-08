7785ce8967b1b16617cde3b21b13b4e7d0bc8e32
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-08 00:01:19 -0500

    Day 5: Part 1 — Simplify

--- lib/05.ex
+++ lib/05.ex
@@ -1,45 +1,31 @@
 defmodule AdventOfCode.Day05 do
   def solve_p1(filename) do
-    {ordering_rules, updates} = read_input(filename)
+    {rules, updates} = read_input(filename)
 
     updates
-    |> Enum.filter(&correct_order?(&1, ordering_rules))
+    |> Enum.filter(&correct_order?(rules, &1))
     |> Enum.map(fn list -> Enum.at(list, Kernel.div(length(list), 2)) end)
     |> Enum.sum()
   end
 
   def solve_p2(filename) do
-    {ordering_rules, updates} = read_input(filename)
+    {rules, updates} = read_input(filename)
 
     updates
-    |> Enum.filter(fn update -> not correct_order?(update, ordering_rules) end)
-    |> Enum.map(fn update -> fix_order(update, ordering_rules) end)
+    |> Enum.filter(fn update -> not correct_order?(update, rules) end)
     |> Enum.map(fn list -> Enum.at(list, Kernel.div(length(list), 2)) end)
     |> Enum.sum()
   end
 
-  def correct_order?([head | tail], ordering_rules) do
-    correct_order?(tail, ordering_rules, MapSet.new([head]))
+  def correct_order?(rules, [first | rest] = _update) do
+    correct_order?(rules, [first], rest)
   end
 
-  defp correct_order?([], _ordering_rules, _current_order), do: true
+  def correct_order?(_rules, _in_order, []), do: true
 
-  defp correct_order?([head | tail], ordering_rules, current_order) do
-    case ordering_rules[head] != nil and MapSet.subset?(current_order, ordering_rules[head]) do
-      true -> correct_order?(tail, ordering_rules, MapSet.put(current_order, head))
-      false -> false
-    end
-  end
-
-  defp fix_order([head | tail], ordering_rules) do
-    fix_order(tail, ordering_rules, MapSet.new([head]))
-  end
-
-  defp fix_order([], _rules, _current), do: true
-
-  defp fix_order([head | tail], ordering_rules, current_order) do
-    case ordering_rules[head] != nil and MapSet.subset?(current_order, ordering_rules[head]) do
-      true -> fix_order(tail, ordering_rules, MapSet.put(current_order, head))
+  def correct_order?(rules, [top | rest_in_order], [next | rest]) do
+    case rules[next] != nil and MapSet.member?(rules[next], top) do
+      true -> correct_order?(rules, [next, top | rest_in_order], rest)
       false -> false
     end
   end
@@ -50,7 +36,7 @@ defmodule AdventOfCode.Day05 do
       |> File.read!()
       |> String.split("\n\n", trim: true)
 
-    ordering_rules =
+    rules =
       part1
       |> String.split("\n", trim: true)
       |> Enum.map(fn line ->
@@ -73,6 +59,6 @@ defmodule AdventOfCode.Day05 do
         |> Enum.map(&String.to_integer/1)
       end)
 
-    {ordering_rules, updates}
+    {rules, updates}
   end
 end
