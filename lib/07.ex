2f289ac8a42e6c024cc5aa1fb88341c0b2818de2
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-07 23:27:57 -0500

    Day 7: Part 1

---
+++ lib/07.ex
@@ -0,0 +1,36 @@
+defmodule AdventOfCode.Day07 do
+  def solve_p1(filename) do
+    read_input(filename)
+    |> Enum.filter(&solvable?/1)
+    |> Enum.map(fn {target, _} -> target end)
+    |> Enum.sum()
+  end
+
+  def solve_p2(filename) do
+    read_input(filename)
+    1
+  end
+
+  def solvable?({target, nums}) do
+    solvable?(target, 0, nums)
+  end
+
+  def solvable?(target, current, []), do: target == current
+  def solvable?(target, current, _nums) when current > target, do: false
+  def solvable?(target, current, [top | rest]) do
+    solvable?(target, current * top, rest) or
+      solvable?(target, current + top, rest)
+  end
+
+  defp read_input(filename) do
+    filename
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [first, rest] = String.split(line, ":", trim: true)
+      target = String.to_integer(first)
+      nums = rest |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
+      {target, nums}
+    end)
+  end
+end
---
+++ test/07_test.exs
@@ -0,0 +1,23 @@
+defmodule AdventOfCode.Day07Test do
+  use ExUnit.Case
+  alias AdventOfCode.Day07
+
+  test "p1: basic" do
+    assert Day07.solve_p1("test/07_tc_01.input") == 3749
+  end
+
+  @tag :skip
+  test "p1: puzzle" do
+    assert Day07.solve_p1("test/07_tc_puzzle.input") == 1
+  end
+
+  @tag :skip
+  test "p2: basic" do
+    assert Day07.solve_p2("test/07_tc_02.input") == 1
+  end
+
+  @tag :skip
+  test "p2: puzzle" do
+    assert Day07.solve_p2("test/07_tc_puzzle.input") == 1
+  end
+end
