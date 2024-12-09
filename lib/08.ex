d8dba45d12030baaef2e6b3d2135abedff2a72d0
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-08 22:31:15 -0700

    Day 8: Part 2

--- lib/08.ex
+++ lib/08.ex
@@ -3,20 +3,31 @@ defmodule AdventOfCode.Day08 do
     {nodes, rows, cols} = read_input(filename)
 
     nodes
-    |> Enum.flat_map(fn {_, positions} -> gen_antinodes(positions) end)
+    |> Enum.flat_map(fn {_, positions} ->
+      positions
+      |> make_pairs()
+      |> Enum.flat_map(fn {a, b} -> place_antinodes(a, b) end)
+    end)
     |> MapSet.new()
-    |> Enum.count(fn {x, y} -> x >= 0 and x < cols and y >= 0 and y < rows end)
+    |> Enum.count(fn {x, y} -> x >= 0 and x < rows and y >= 0 and y < cols end)
   end
 
   def solve_p2(filename) do
-    read_input(filename)
-    1
-  end
+    {nodes, rows, cols} = read_input(filename)
 
-  def gen_antinodes(positions) do
-    positions
-    |> make_pairs()
-    |> Enum.flat_map(fn {a, b} -> place_antinodes(a, b) end)
+    nodes
+    |> Enum.flat_map(fn {_, positions} ->
+      positions
+      |> make_pairs()
+      |> Enum.flat_map(fn {{a, b}, {c, d}} ->
+        {dx, dy} = {a - c, b - d}
+
+        place_antinodes_along_line({a, b}, {dx, dy}, rows, cols) ++
+          place_antinodes_along_line({c, d}, {-dx, -dy}, rows, cols)
+      end)
+    end)
+    |> MapSet.new()
+    |> Enum.count()
   end
 
   def make_pairs(positions) do
@@ -26,10 +37,17 @@ defmodule AdventOfCode.Day08 do
   end
 
   def place_antinodes({a, b}, {c, d}) do
-    xdiff = a - c
-    ydiff = b - d
+    {dx, dy} = {a - c, b - d}
+    [{a + dx, b + dy}, {c - dx, d - dy}]
+  end
 
-    [{a + xdiff, b + ydiff}, {c - xdiff, d - ydiff}]
+  def place_antinodes_along_line({a, b}, _, rows, cols)
+      when a < 0 or a >= cols or b < 0 or b >= rows,
+      do: []
+
+  def place_antinodes_along_line({a, b}, {dx, dy}, rows, cols) do
+    new_antinode = {a + dx, b + dy}
+    [{a, b} | place_antinodes_along_line(new_antinode, {dx, dy}, rows, cols)]
   end
 
   defp read_input(filename) do
@@ -45,11 +63,11 @@ defmodule AdventOfCode.Day08 do
     nodes =
       chars
       |> Enum.with_index()
-      |> Enum.flat_map(fn {line, y} ->
+      |> Enum.flat_map(fn {line, x} ->
         line
         |> Enum.with_index()
         |> Enum.filter(fn {antenna, _} -> antenna != "." end)
-        |> Enum.map(fn {antenna, x} -> {antenna, {x, y}} end)
+        |> Enum.map(fn {antenna, y} -> {antenna, {x, y}} end)
       end)
       |> Enum.group_by(fn {k, _} -> k end, fn {_, v} -> v end)
       |> Enum.filter(fn {_, v} -> length(v) > 1 end)
--- test/08_test.exs
+++ test/08_test.exs
@@ -3,20 +3,26 @@ defmodule AdventOfCode.Day08Test do
   alias AdventOfCode.Day08
 
   test "p1: basic" do
-    assert Day08.solve_p1("test/08_tc_01.input") == 14
+    assert Day08.solve_p1("test/08_tc_01.input") == 3
+  end
+
+  test "p1: more antennae" do
+    assert Day08.solve_p1("test/08_tc_02.input") == 14
   end
 
   test "p1: puzzle" do
     assert Day08.solve_p1("test/08_tc_puzzle.input") == 247
   end
 
-  @tag :skip
   test "p2: basic" do
-    assert Day08.solve_p2("test/08_tc_01.input") == 1
+    assert Day08.solve_p2("test/08_tc_01.input") == 9
   end
 
-  @tag :skip
+  test "p2: more antennae" do
+    assert Day08.solve_p2("test/08_tc_02.input") == 34
+  end
+
   test "p2: puzzle" do
-    assert Day08.solve_p2("test/08_tc_puzzle.input") == 1
+    assert Day08.solve_p2("test/08_tc_puzzle.input") == 861
   end
 end
