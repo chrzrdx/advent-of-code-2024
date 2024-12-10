e79cb95b13c12fe8fb7328ee0566c980b9911678
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-09 19:31:21 -0700

    Day 9: Part 1 refactor

--- lib/09.ex
+++ lib/09.ex
@@ -1,6 +1,6 @@
 defmodule AdventOfCode.Day09 do
   def solve_p1(filename) do
-    blocks = read_input(filename)
+    blocks = filename |> read_input() |> expand()
 
     total_used = Enum.count(blocks, &(&1 != :free))
     compacted_blocks = blocks |> Enum.take(total_used)
@@ -16,6 +16,13 @@ defmodule AdventOfCode.Day09 do
   def solve_p2(filename) do
     read_input(filename)
     1
+  end
+
+  def expand(compacted) do
+    compacted
+    |> Enum.flat_map(fn {num, block} ->
+      for _ <- 0..(num - 1)//1, do: block
+    end)
   end
 
   def merge(blocks, fill_blocks) do
@@ -40,17 +47,12 @@ defmodule AdventOfCode.Day09 do
     |> File.read!()
     |> String.split("", trim: true)
     |> Enum.map(&String.to_integer/1)
-    |> Enum.chunk_every(2)
     |> Enum.with_index()
-    |> Enum.flat_map(fn
-      {[used, free], file_index} ->
-        used_blocks = Enum.map(0..(used - 1)//1, fn _ -> file_index end)
-        free_blocks = Enum.map(0..(free - 1)//1, fn _ -> :free end)
-        used_blocks ++ free_blocks
-
-      {[used], file_index} ->
-        used_blocks = Enum.map(0..(used - 1)//1, fn _ -> file_index end)
-        used_blocks
+    |> Enum.map(fn {num_blocks, index} ->
+      case Kernel.rem(index, 2) do
+        0 -> {num_blocks, Kernel.div(index, 2)}
+        _ -> {num_blocks, :free}
+      end
     end)
   end
 end
