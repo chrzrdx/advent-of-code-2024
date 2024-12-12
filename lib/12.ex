6ab324cb64955783cf77bb24b3f1479fac92b9a1
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-12 01:03:49 -0700

    Day 12 Part 1 Refactor

--- lib/12.ex
+++ lib/12.ex
@@ -1,9 +1,9 @@
 defmodule AdventOfCode.Day12 do
   def solve_p1(filename) do
-    {map, vertices} = read_input(filename)
-
-    all_regions(map, vertices)
-    |> Enum.map(&(area(&1) * perimeter(&1)))
+    filename
+    |> read_input()
+    |> then(&find_regions(&1, Map.keys(&1), []))
+    |> Enum.map(&region_score/1)
     |> Enum.sum()
   end
 
@@ -12,60 +12,49 @@ defmodule AdventOfCode.Day12 do
     1
   end
 
-  def all_regions(map, to_visit, regions \\ []) do
-    case MapSet.size(to_visit) do
-      0 ->
-        regions
+  def find_regions(_map, [], regions), do: regions
 
-      _ ->
-        current = Enum.at(to_visit, 0)
-        r = region(map, [current], %{})
-
-        visited = Map.keys(r) |> MapSet.new()
-        remaining_to_visit = MapSet.difference(to_visit, visited)
-
-        all_regions(map, remaining_to_visit, [r | regions])
-    end
+  def find_regions(map, [current | queue], regions) do
+    region = flood_fill(map, [current], %{})
+    find_regions(map, queue -- Map.keys(region), [region | regions])
   end
 
-  def region(_, [], visited), do: visited
+  def flood_fill(_, [], visited), do: visited
 
-  def region(map, [current | to_visit], visited) do
-    adjacent = connected(map, current)
-    unvisited = Enum.filter(adjacent, &(!Map.has_key?(visited, &1))) ++ to_visit
-    updated_visited = Map.put(visited, current, length(adjacent))
-    region(map, unvisited, updated_visited)
+  def flood_fill(map, [pos | queue], visited) do
+    neighbours = get_neighbours(map, pos)
+    new_queue = (neighbours -- Map.keys(visited)) ++ queue
+    new_visited = Map.put(visited, pos, length(neighbours))
+    flood_fill(map, new_queue, new_visited)
   end
 
-  def connected(map, {x, y}) do
+  def get_neighbours(map, {x, y}) do
     [{x, y - 1}, {x, y + 1}, {x - 1, y}, {x + 1, y}]
-    |> Enum.filter(fn pos -> map[pos] == map[{x, y}] end)
+    |> Enum.filter(&(map[&1] == map[{x, y}]))
   end
 
-  def area(region), do: length(Map.keys(region))
+  def region_score(region), do: area(region) * perimeter(region)
+
+  def area(region), do: map_size(region)
 
   def perimeter(region) do
-    Map.values(region)
-    |> Enum.map(&(4 - &1))
+    region
+    |> Map.values()
     |> Enum.sum()
+    |> then(&(4 * map_size(region) - &1))
   end
 
   defp read_input(filename) do
-    map =
-      filename
-      |> File.read!()
-      |> String.split("\n", trim: true)
+    filename
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, x} ->
+      line
+      |> String.split("", trim: true)
       |> Enum.with_index()
-      |> Enum.flat_map(fn {line, x} ->
-        line
-        |> String.split("", trim: true)
-        |> Enum.with_index()
-        |> Enum.map(fn {char, y} -> {{x, y}, char} end)
-      end)
-      |> Map.new()
-
-    vertices = MapSet.new(Map.keys(map))
-
-    {map, vertices}
+      |> Enum.map(fn {char, y} -> {{x, y}, char} end)
+    end)
+    |> Map.new()
   end
 end
