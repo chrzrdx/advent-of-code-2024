e60193be1e523b6fb9b3b7cc9acc81f0781bbc3b
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-11 17:36:16 -0700

    Day 11 refactor

--- lib/11.ex
+++ lib/11.ex
@@ -1,18 +1,12 @@
 defmodule AdventOfCode.Day11 do
-  def solve_p1(filename) do
+  def solve_p1(filename), do: solve(filename, 25)
+
+  def solve_p2(filename), do: solve(filename, 75)
+
+  def solve(filename, iterations) do
     nums = read_input(filename)
 
-    Enum.reduce(1..25, nums, fn _, acc ->
-      blink_nums(acc)
-    end)
-    |> Map.values()
-    |> Enum.sum()
-  end
-
-  def solve_p2(filename) do
-    nums = read_input(filename)
-
-    Enum.reduce(1..75, nums, fn _, acc ->
+    Enum.reduce(1..iterations, nums, fn _, acc ->
       blink_nums(acc)
     end)
     |> Map.values()
@@ -21,12 +15,11 @@ defmodule AdventOfCode.Day11 do
 
   def blink_nums(nums) do
     Enum.flat_map(nums, fn {num, count} ->
-      blink(num)
-      |> Enum.map(fn n -> {n, count} end)
+      blink(num) |> Enum.map(fn n -> {n, count} end)
     end)
-    |> Enum.group_by(fn {k, _} -> k end, fn {_, v} -> v end)
-    |> Enum.map(fn {k, v} -> {k, Enum.sum(v)} end)
-    |> Enum.into(%{})
+    |> Enum.reduce(%{}, fn {n, count}, acc ->
+      Map.update(acc, n, count, &(&1 + count))
+    end)
   end
 
   def blink(n) when n == 0, do: [1]
@@ -35,11 +28,11 @@ defmodule AdventOfCode.Day11 do
     string = "#{n}"
     num_digits = String.length(string)
 
-    case Kernel.rem(num_digits, 2) do
+    case rem(num_digits, 2) do
       0 ->
-        String.split_at(string, div(num_digits, 2))
-        |> Tuple.to_list()
-        |> Enum.map(&String.to_integer/1)
+        string
+        |> String.split_at(div(num_digits, 2))
+        |> then(fn {a, b} -> [String.to_integer(a), String.to_integer(b)] end)
 
       1 ->
         [n * 2024]
