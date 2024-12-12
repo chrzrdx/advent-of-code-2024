0f8ff3b4f0c27cd301b9e4558f7be6ec15e27f02
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-12 15:04:44 -0700

    Day 12: Part 2 tests

--- test/12_test.exs
+++ test/12_test.exs
@@ -18,9 +18,28 @@ defmodule AdventOfCode.Day12Test do
     assert Day12.solve_p1("test/12_tc_puzzle.input") == 1_387_004
   end
 
+  test "p2: basic 1" do
+    assert Day12.solve_p2("test/12_tc_01.input") == 80
+  end
+
   @tag :skip
-  test "p2: basic" do
-    assert Day12.solve_p2("test/12_tc_02.input") == 1
+  test "p2: basic 2" do
+    assert Day12.solve_p2("test/12_tc_02.input") == 436
+  end
+
+  @tag :skip
+  test "p2: basic 3" do
+    assert Day12.solve_p2("test/12_tc_03.input") == 1206
+  end
+
+  @tag :skip
+  test "p2: basic 4" do
+    assert Day12.solve_p2("test/12_tc_04.input") == 236
+  end
+
+  @tag :skip
+  test "p2: basic 5" do
+    assert Day12.solve_p2("test/12_tc_05.input") == 368
   end
 
   @tag :skip
