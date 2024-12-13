2b1f044d4355b1fd94a2b93fa0e792c9b9ca3e46
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-12 17:11:52 -0700

    Day 12: Part 2

--- lib/12.ex
+++ lib/12.ex
@@ -8,8 +8,57 @@ defmodule AdventOfCode.Day12 do
   end
 
   def solve_p2(filename) do
-    read_input(filename)
-    1
+    filename
+    |> read_input()
+    |> then(&find_regions(&1, Map.keys(&1), []))
+    |> Enum.map(fn region ->
+      lines =
+        [:up, :down, :left, :right]
+        |> Enum.map(&count_segments(region, &1))
+        |> Enum.sum()
+
+      area(region) * lines
+    end)
+    |> Enum.sum()
+  end
+
+  def count_segments(region, dir) do
+    ordered =
+      region
+      |> Map.filter(fn {_, v} -> dir not in v end)
+      |> Map.keys()
+
+    case dir do
+      :up -> ordered |> Enum.sort()
+      :down -> ordered |> Enum.sort()
+      :left -> ordered |> Enum.sort_by(fn {x, y} -> {y, x} end)
+      :right -> ordered |> Enum.sort_by(fn {x, y} -> {y, x} end)
+    end
+    |> do_count_segments([], dir)
+    |> then(&length/1)
+  end
+
+  def do_count_segments([], [], _dir), do: []
+  def do_count_segments([], groups, _dir), do: groups
+  def do_count_segments([top | blocks], [], dir), do: do_count_segments(blocks, [[top]], dir)
+
+  def do_count_segments([curr | blocks], [line | lines], dir) do
+    top = hd(line)
+
+    if connected?(curr, top, dir) do
+      do_count_segments(blocks, [[curr | line] | lines], dir)
+    else
+      do_count_segments(blocks, [[curr], line | lines], dir)
+    end
+  end
+
+  def connected?({x, y}, {x2, y2}, dir) do
+    case dir do
+      :up -> x == x2 and y2 == y - 1
+      :down -> x == x2 and y2 == y - 1
+      :left -> x2 == x - 1 and y == y2
+      :right -> x2 == x - 1 and y == y2
+    end
   end
 
   def find_regions(_map, [], regions), do: regions
@@ -22,15 +71,15 @@ defmodule AdventOfCode.Day12 do
   def flood_fill(_, [], visited), do: visited
 
   def flood_fill(map, [pos | queue], visited) do
-    neighbours = get_neighbours(map, pos)
+    {neighbours, connections} = get_neighbours(map, pos) |> Enum.unzip()
     new_queue = (neighbours -- Map.keys(visited)) ++ queue
-    new_visited = Map.put(visited, pos, length(neighbours))
+    new_visited = Map.put(visited, pos, connections)
     flood_fill(map, new_queue, new_visited)
   end
 
   def get_neighbours(map, {x, y}) do
-    [{x, y - 1}, {x, y + 1}, {x - 1, y}, {x + 1, y}]
-    |> Enum.filter(&(map[&1] == map[{x, y}]))
+    [{{x, y - 1}, :left}, {{x, y + 1}, :right}, {{x - 1, y}, :up}, {{x + 1, y}, :down}]
+    |> Enum.filter(fn {pos, _} -> map[pos] == map[{x, y}] end)
   end
 
   def region_score(region), do: area(region) * perimeter(region)
@@ -40,6 +89,7 @@ defmodule AdventOfCode.Day12 do
   def perimeter(region) do
     region
     |> Map.values()
+    |> Enum.map(&length/1)
     |> Enum.sum()
     |> then(&(4 * map_size(region) - &1))
   end
--- test/12_test.exs
+++ test/12_test.exs
@@ -22,28 +22,23 @@ defmodule AdventOfCode.Day12Test do
     assert Day12.solve_p2("test/12_tc_01.input") == 80
   end
 
-  @tag :skip
   test "p2: basic 2" do
     assert Day12.solve_p2("test/12_tc_02.input") == 436
   end
 
-  @tag :skip
   test "p2: basic 3" do
     assert Day12.solve_p2("test/12_tc_03.input") == 1206
   end
 
-  @tag :skip
   test "p2: basic 4" do
     assert Day12.solve_p2("test/12_tc_04.input") == 236
   end
 
-  @tag :skip
   test "p2: basic 5" do
     assert Day12.solve_p2("test/12_tc_05.input") == 368
   end
 
-  @tag :skip
   test "p2: puzzle" do
-    assert Day12.solve_p2("test/12_tc_puzzle.input") == 1
+    assert Day12.solve_p2("test/12_tc_puzzle.input") == 844_198
   end
 end
