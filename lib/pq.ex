81407785cd9e1dbee1ea267e94e3872373078038
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-24 11:45:24 -0700

    Comment out WIP priority queue

--- lib/pq.ex
+++ lib/pq.ex
@@ -15,7 +15,9 @@ defmodule PriorityQueue do
 
   def pop(%PriorityQueue{tree: tree}) do
     case :gb_trees.is_empty(tree) do
-      true -> {:error, :empty_queue}
+      true ->
+        {:error, :empty_queue}
+
       false ->
         {min_priority, min_value, new_tree} = :gb_trees.take_smallest(tree)
         {:ok, {min_value, min_priority}, %PriorityQueue{tree: new_tree}}
@@ -25,6 +27,14 @@ defmodule PriorityQueue do
   def is_empty(%PriorityQueue{tree: tree}), do: :gb_trees.is_empty(tree)
 end
 
-p = PriorityQueue
-q = p.new() |> p.insert(:a, 3) |> p.insert(:b, 4) |> p.insert(:c, 2) |> p.insert(:d, 5) |> p.insert(:e, 1)
-p.pop(q)
+# p = PriorityQueue
+
+# q =
+#   p.new()
+#   |> p.insert(:a, 3)
+#   |> p.insert(:b, 4)
+#   |> p.insert(:c, 2)
+#   |> p.insert(:d, 5)
+#   |> p.insert(:e, 1)
+
+# p.pop(q)
