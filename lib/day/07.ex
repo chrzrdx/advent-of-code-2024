276b3c82e9739f6ea8deb3b6fe51a09521f1de15
Abhinandan Panigrahi <chrzrdx@gmail.com>
2024-12-24 22:27:02 -0700

    Refactor Day 07 to parallelize processing

--- lib/day/07.ex
+++ lib/day/07.ex
@@ -4,17 +4,25 @@ defmodule AdventOfCode.Day07 do
   def solve_p1(filename) do
     filename
     |> read_input()
-    |> Enum.filter(fn equation -> Equation.solvable?(equation, [&+/2, &*/2]) end)
-    |> Enum.sum_by(& &1.target)
+    |> Task.async_stream(
+      fn eq ->
+        if Equation.solvable?(eq, [&+/2, &*/2]), do: eq.target, else: 0
+      end,
+      ordered: false
+    )
+    |> Enum.sum_by(fn {:ok, value} -> value end)
   end
 
   def solve_p2(filename) do
     filename
     |> read_input()
-    |> Enum.filter(fn equation ->
-      Equation.solvable?(equation, [&+/2, &*/2, &Equation.concat/2])
-    end)
-    |> Enum.sum_by(& &1.target)
+    |> Task.async_stream(
+      fn eq ->
+        if Equation.solvable?(eq, [&+/2, &*/2, &Equation.concat/2]), do: eq.target, else: 0
+      end,
+      ordered: false
+    )
+    |> Enum.sum_by(fn {:ok, value} -> value end)
   end
 
   defp read_input(filename) do
